0008df653a685d61d941ac8df6737a5160ea1cae
Mist <hatersparty@gmail.com>
2018-08-07 13:26:48 +0300

    [new] Article model

--- aggregator/newscollector/models.py
+++ aggregator/newscollector/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    '''
+    Модель новости
+    '''
+    title = models.CharField(
+        max_length=255,
+        blank=True,
+        default='NewsTitle')
+    content = models.TextField(
+        blank=True)
+    thumbnail = models.CharField(
+        max_length=255,
+        blank=True)
+    origin_link = models.CharField(
+        max_length=255,
+        blank=True)
