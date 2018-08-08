9729303e6e3e6fcfdd05a1ce8933ab58388ad35c
Mist <hatersparty@gmail.com>
2018-08-08 14:34:40 +0300

    [update] database model

--- aggregator/newscollector/models.py
+++ aggregator/newscollector/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Article(models.Model):
     '''
-    Модель новости
+    News model
     '''
     title = models.CharField(
         max_length=255,
@@ -11,9 +11,21 @@ class Article(models.Model):
         default='NewsTitle')
     content = models.TextField(
         blank=True)
+    content_short = models.TextField(
+        blank=True)
+    # Article image link
     thumbnail = models.CharField(
         max_length=255,
         blank=True)
+    # Page from where rss came from
     origin_link = models.CharField(
         max_length=255,
         blank=True)
+    # Page where original article can be found
+    link = models.CharField(
+        max_length=255)
+    date = models.DateTimeField()
+
+    class Meta:
+        ordering = ['-date']
+        unique_together = ('title', 'date', 'origin_link', 'link')
